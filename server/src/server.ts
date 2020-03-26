041a97f1bcdf57adf00605f437f04538fe2ed9b3
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-03-26 23:57:57 +0300

    Update server starting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,21 @@
 import App from './app';
+import config from 'config';
+// import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.get('port') || 3000;
 
-App.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
-});
+async function start(): Promise<void> {
+  try {
+    // await mongoose.connect(config.get('mongoUri'), {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true
+    // });
+    App.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`));
+  } catch (e) {
+    console.log('Server error:', e.message);
+    process.exit(1);
+  }
+}
+
+start();
