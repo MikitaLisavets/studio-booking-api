88b1bddb6a78beb63a6d73b2b74eac88861b5106
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-04-10 17:48:31 +0300

    Add default middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const App: Application = express();
+
+// middlewares
+App.use(cors());
+App.use(bodyParser.json());
 
 App.get('/', (req: Request, res: Response) => {
   res.send('Full-stack App Starter');
