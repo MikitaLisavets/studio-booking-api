237d69dd090571e27f3010686d6146b148d44526
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-04-10 17:49:03 +0300

    Update mongodb config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,18 @@
 import App from './app';
 import dotenv from 'dotenv';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 async function start(): Promise<void> {
   try {
-    // await mongoose.connect(process.env.MONGODB_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true
-    // });
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
     App.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`));
   } catch (e) {
     console.log('Server error:', e.message);
