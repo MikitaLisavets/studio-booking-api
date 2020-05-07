a4e386db95093f37059aaabca24f6db104ca71ef
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-05-07 19:15:11 +0300

    Remove useless async

--- src/routes/signUp.test.js
+++ src/routes/signUp.test.js
@@ -18,11 +18,11 @@ describe('signUpRoute', () => {
       response = await request(app).post('/');
     });
 
-    it('returns status code 400', async () => {
+    it('returns status code 400', () => {
       expect(response.status).toEqual(400);
     });
 
-    it('returns error', async () => {
+    it('returns error', () => {
       expect(response.body).toEqual({ message: 'errorMessage', code: 'errorCode', statusCode: 400 });
     });
   });
@@ -32,11 +32,11 @@ describe('signUpRoute', () => {
       response = await request(app).post('/').send({ email: 'email', password: 'password' });
     });
 
-    it('returns status code 200', async () => {
+    it('returns status code 200', () => {
       expect(response.status).toEqual(200);
     });
 
-    it('returns unconfirmed user', async () => {
+    it('returns unconfirmed user', () => {
       expect(response.body).toEqual({ UserConfirmed: false });
     });
   });
