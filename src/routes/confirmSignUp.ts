917dd4e44a975140bbfe484040753d015ad8bd0b
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-05-28 20:13:14 +0300

    Set max cookie age

--- src/routes/confirmSignUp.ts
+++ src/routes/confirmSignUp.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { confirmSignUp, getToken, getUser } from '../services/cognito';
 import { InitiateAuthResponse, GetUserResponse } from 'aws-sdk/clients/cognitoidentityserviceprovider';
 import convertAttributesToUser from '../utils/convertAttributesToUser';
-import { COOKIE_TOKEN } from '../constants';
+import { COOKIE_TOKEN, MAX_COOKIE_AGE } from '../constants';
 
 const router = express.Router();
 
@@ -13,7 +13,7 @@ router.post('/', (req: Request, res: Response) => {
     .then(() => getToken({ email, password })
       .then((tokens: InitiateAuthResponse) => getUser({ accessToken: tokens.AuthenticationResult?.AccessToken })
         .then((data: GetUserResponse) => {
-          res.cookie(COOKIE_TOKEN, tokens.AuthenticationResult?.RefreshToken, { httpOnly: true }).send({
+          res.cookie(COOKIE_TOKEN, tokens.AuthenticationResult?.RefreshToken, { httpOnly: true, maxAge: MAX_COOKIE_AGE }).send({
             user: convertAttributesToUser(data.UserAttributes)
           });
         })
