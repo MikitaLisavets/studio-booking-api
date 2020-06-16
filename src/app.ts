5fbeded8591cad730a6c295f028820688632b168
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-16 21:04:57 +0300

    Remove dead code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import DB from './services/dynamodb';
 import signUpRouter from './routes/signUp';
 import confirmSignUpRouter from './routes/confirmSignUp';
 import getAdminUserRouter from './routes/getAdminUser';
@@ -29,13 +28,6 @@ App.use('/login', loginRouter);
 App.use('/updateSession', updateSessionRouter);
 App.use('/logout', logoutRouter);
 
-// DynamoDB
-App.post('/getDataFromDB', async (req: Request, res: Response) => {
-  const ID = req.body.id;
-  const data = await DB.get(ID).catch(err => err);
-
-  res.send(data);
-});
 
 App.get('/test', (req: Request, res: Response) => {
   res.send('Test response');
