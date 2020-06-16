0d319701658ce29a08c9340e1ec9270cdbf68c3e
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-16 21:10:43 +0300

    Save user to dynamoDB on signup

--- src/routes/confirmSignUp.ts
+++ src/routes/confirmSignUp.ts
@@ -4,6 +4,7 @@ import { InitiateAuthResponse, GetUserResponse } from 'aws-sdk/clients/cognitoid
 import convertAttributesToUser from '../utils/convertAttributesToUser';
 import { COOKIE_TOKEN, MAX_COOKIE_AGE } from '../constants';
 import { defaultErrorHandler } from '../utils/helpers';
+import { putUserToDB } from '../services/usersDB';
 
 const router = express.Router();
 
@@ -14,10 +15,13 @@ router.post('/', (req: Request, res: Response) => {
     .then(() => getToken({ email, password })
       .then((tokens: InitiateAuthResponse) => getUser({ accessToken: tokens.AuthenticationResult?.AccessToken })
         .then((data: GetUserResponse) => {
-          res.cookie(COOKIE_TOKEN, tokens.AuthenticationResult?.RefreshToken, { httpOnly: true, maxAge: MAX_COOKIE_AGE });
-          res.send({
-            user: convertAttributesToUser(data.UserAttributes)
-          });
+          const user = convertAttributesToUser(data.UserAttributes);
+          putUserToDB(data.Username, user)
+            .then(() => {
+              res.cookie(COOKIE_TOKEN, tokens.AuthenticationResult?.RefreshToken, { httpOnly: true, maxAge: MAX_COOKIE_AGE });
+              res.send({ user });
+            })
+            .catch((error) => defaultErrorHandler(res, error));
         })
         .catch((error) => defaultErrorHandler(res, error)))
       .catch((error) => defaultErrorHandler(res, error)))
