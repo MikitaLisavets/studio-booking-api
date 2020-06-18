f405a987f5a330ae41a102f5a7e18a60aea88004
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-18 14:54:36 +0300

    Remove express start script

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,4 @@ dotenv.config();
 import App from './app';
 import serverless from 'serverless-http';
 
-if (process.env.NODE_ENV === 'express') {
-  const PORT = process.env.PORT;
-  App.listen(process.env.PORT, () => console.log(`Server is running in http://localhost:${PORT}`));
-}
-
 module.exports.handler = serverless(App);
